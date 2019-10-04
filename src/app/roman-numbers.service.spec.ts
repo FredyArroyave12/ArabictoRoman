207ac83fc48d2473fbcdf6003a21fafa1d448e3b
FredyArroyave12 <FredyArroyave@udea.edu.co>
2019-10-04 00:40:24 -0500

    ---

--- src/app/roman-numbers.service.spec.ts
+++ src/app/roman-numbers.service.spec.ts
@@ -24,4 +24,31 @@ describe('RomanNumbersService', () => {
   it('4 debe ser igual a IV', inject([RomanNumbersService], (service: RomanNumbersService) => {
     expect(service.arabtoroman(4)).toEqual('IV');
   }));
+  it('5 debe ser igual a V', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(5)).toEqual('V');
+  }));
+  it('6 debe ser igual a VI', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(6)).toEqual('VI');
+  }));
+  it('7 debe ser igual a VII', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(7)).toEqual('VII');
+  }));
+  it('9 debe ser igual a IX', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(9)).toEqual('IX');
+  }));
+  it('15 debe ser igual a XV', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(15)).toEqual('XV');
+  }));
+  it('49 debe ser igual a XLIX', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(49)).toEqual('XLIX');
+  }));
+  it('65 debe ser igual a LXV', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(65)).toEqual('LXV');
+  }));
+  it('457 debe ser igual a CDLVII', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(457)).toEqual('CDLVII');
+  }));
+  it('940 debe ser igual a CMXL', inject([RomanNumbersService], (service: RomanNumbersService) => {
+    expect(service.arabtoroman(940)).toEqual('CMXL');
+  }));
 });
--- src/app/roman-numbers.service.ts
+++ src/app/roman-numbers.service.ts
@@ -8,16 +8,109 @@ export class RomanNumbersService {
 
   constructor() { }
 
-  arabtoroman(number: Number){
-    if (number < 4){
+  arabtoroman(numb: Number){
+    if (numb < 4){
       let roman: String = "";
-      for (let i=0; i<number; i++ ){
+      for (let i=0; i<numb; i++ ){
           roman +="I"
       }
-          return roman;
+      return roman;
     }
-    if(number = 4){
+    
+    if(numb == 4){
       return 'IV'
     }
+
+    if (numb == 5) {
+      return "V";
+    }
+    if (numb > 5 && numb < 9) {
+      let roman: String = ""
+      for (let i=5; i< numb; i++ ){
+        roman +="I"
+      }
+        return "V" + roman;
+    }
+
+    if (numb == 9) {
+      return "IX";
+    }
+
+    if (numb == 10) {
+      return "X";
+    }
+
+    if (numb > 10 && numb < 100 && numb != 40 && numb != 90 && numb != 50) {
+      if (numb % 10 == 0) {
+        if (numb / 10 > 5) {
+          return 'L' + this.aisgnaX(numb - 50);
+        }
+        return this.aisgnaX(numb);
+      } else if (numb % 10 == 4 || numb % 10 == 9) {
+        var aux = numb % 10;
+        aux = numb - aux;
+        return this.arabtoroman(aux) + this.arabtoroman(numb % 10);
+      } else {
+        var aux = numb % 10;
+        numb = numb - aux;
+        return this.arabtoroman(numb) + this.arabtoroman(aux);
+      }
+    }
+    if (numb > 100 && numb < 1000 && numb != 400 && numb != 500 && numb != 900) {
+      if (numb % 100 == 0) {
+        if (numb / 100 > 5) {
+          return 'D' + this.aisgnaC(numb - 500);
+        }
+        return this.aisgnaC(numb);
+      } else if (numb % 100 == 40 || numb % 100 == 90) {
+        var aux = numb % 100;
+        aux = numb - aux;
+        return this.arabtoroman(aux) + this.arabtoroman(numb % 100);
+      } else{
+        var aux = numb % 100;
+        numb = numb - aux;
+        return this.arabtoroman(numb) + this.arabtoroman(aux);
+      }
+    }
+    if (numb == 40) {
+      return "XL";
+    }
+    if (numb == 50) {
+      return "L";
+    }
+    if (numb == 90) {
+      return "XC";
+    }
+    if (numb == 100) {
+      return "C";
+    }
+    if (numb == 400) {
+      return "CD";
+    }
+    if (numb == 500) {
+      return "D";
+    }
+    if (numb == 900) {
+      return "CM";
+    }
+    if (numb == 1000) {
+      return "M";
+    }
   } 
+  aisgnaX(numb: number) {
+    let roman: string = "";
+    for (let i = 0; i < numb; i += 10) {
+      roman += "X";
+    }
+    return roman;
+  }
+  aisgnaC(numb: Number) {
+    let roman: string = "";
+    for (let i = 0; i < numb; i += 100) {
+      roman += "C";
+    }
+    return roman;
+  }
 }
+
+
